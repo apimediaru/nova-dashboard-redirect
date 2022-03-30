2aeb91e4b4b0bf22db4cc4cb3d68931514fe0f0b
Denis Mamaev <41694699+ExileofAranei@users.noreply.github.com>
2022-03-30 15:09:15 +0300

    Update nova-dashboard-redirect.php

--- config/nova-dashboard-redirect.php
+++ config/nova-dashboard-redirect.php
@@ -6,6 +6,6 @@ return [
      * Redirect configuration. Might be string what would be passed into Vue Router path option,
      * or an array or callable that returns route configuration
      */
-    'redirect' => '/resources/users',
+    'redirect' => false,
 
 ];
